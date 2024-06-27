d49e17f20b6ef1ddf6ff7ddfe7fcd038b5d6e251
Lewis <lewiswernerv@gmail.com>
2024-06-27 17:11:31 -0500

    upgrades flutter & dart removes unused code from main.dart and normalizes all line endings

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:guide_solve/themes/light_mode.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'pages/home_page.dart';
-import 'dart:html' as html;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,53 +24,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  @override
-  void initState() {
-    super.initState();
-    _handleIncomingLinks();
-  }
-
-  void _handleIncomingLinks() async {
-    // Listen for auth state changes
-    _auth.authStateChanges().listen((User? user) {
-      if (user != null) {
-        print("User is signed in!");
-      }
-    });
-
-    final String currentUrl = html.window.location.href;
-
-    // Check if the app was launched with an email link
-    if (_auth.isSignInWithEmailLink(currentUrl)) {
-      _handleEmailLinkSignIn(currentUrl);
-    }
-  }
-
-  void _handleEmailLinkSignIn(String link) async {
-    try {
-      // Retrieve the email from storage or prompt the user
-      final email = await _getEmailFromStorage();
-      if (email != null) {
-        await _auth.signInWithEmailLink(email: email, emailLink: link);
-        print("Successfully signed in with email link!");
-        // Navigate to the Dashboard after successful sign-in
-        Navigator.pushReplacementNamed(context, '/dashboard');
-      } else {
-        print("No email found to sign in with.");
-      }
-    } catch (e) {
-      print("Error signing in with email link: $e");
-    }
-  }
-
-  Future<String?> _getEmailFromStorage() async {
-    // Implement this method to retrieve the stored email
-    // For demonstration, we'll return a hardcoded email
-    return "user@example.com";
-  }
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
